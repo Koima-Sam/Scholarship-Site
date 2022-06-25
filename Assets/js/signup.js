0902a5c6f06638ad3d0a902cf6cbb6c97781dbd9
Koima-Sam <sam17koima@gmail.com>
2022-06-25 20:29:10 +0300

    check inputs of the form

---
+++ Assets/js/signup.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', (e)=>{
+    document.addEventListener('submit', e=>{
+        e.preventDefault()
+        checkInput(e.target)
+    })
+})
+
+//Check if input fields are filled and completed
+function checkInput(form){
+    const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const name = form.fullname.value,
+    email = form.userEmail.value,
+    password = form.userPassword.value,
+    password2 = form.userPassword2.value
+
+    if(name===""){
+        alert("Name field has not been filled")
+        form.fullname.focus()
+    }
+    else if(email ===""||!email.match(mailformat)){
+        alert("Please write a valid email")
+        form.userEmail.focus()
+    }
+    else if(password===""||password2==="" || password!= password2){
+        alert([password.value, password2.value])
+        form.userPassword.focus()
+    }
+
+}
